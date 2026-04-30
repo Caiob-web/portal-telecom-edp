0632d1e5f2f1b282dc49b0a035806618efc3708a
Caioba <caioba41342@outlook.com>
2026-04-30 17:23:44 -0300

    Create next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth'
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string
+      name?: string | null
+      email?: string | null
+      image?: string | null
+      company_name: string
+      role: string
+      municipality?: string
+    }
+  }
+}
