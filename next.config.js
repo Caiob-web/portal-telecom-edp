1cec176c34c5dc55926d256c79b35362c6b1aa69
Caioba <caioba41342@outlook.com>
2026-05-02 12:37:46 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,14 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@neondatabase/serverless'],
   },
+  // Garante que as páginas dinâmicas não sejam pré-renderizadas
+  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
       { protocol: 'https', hostname: '*.vercel-storage.com' },
       { protocol: 'https', hostname: 'www.edp.com.br' },
+      { protocol: 'https', hostname: 'brand.edp.com' },
     ],
   },
 }
